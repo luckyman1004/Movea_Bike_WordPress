2723da7eaa39349ad7e360b94cede079e782efe2
luckyman1004 <luckysuperdev@gmail.com>
2018-03-15 22:43:50 +0200

    change sub title

--- themes/separate-blog/page-coming.php
+++ themes/separate-blog/page-coming.php
@@ -32,7 +32,7 @@ get_header(); ?>
 			<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 form-wrapper">
                 <h4>Stay turned</h4>
                 <h1>Subscribe to our newsletter and be one of the first to benefit from 40% perk via Indiegogo.<br></h1>
-                <h3>The most cool E bikes</h3>
+                <h3>The most stylish E bikes</h3>
                 <?php 
 					while ( have_posts() ) : the_post();
 						the_content(); 
