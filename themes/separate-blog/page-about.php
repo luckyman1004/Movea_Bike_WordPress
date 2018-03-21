07d0aca0b6995bd14c4aae866c02e96ac65c2ff5
luckyman1004 <luckysuperdev@gmail.com>
2018-03-21 19:41:41 +0200

    update about font

--- themes/separate-blog/page-about.php
+++ themes/separate-blog/page-about.php
@@ -21,7 +21,7 @@ get_header(); ?>
 	<div class="container">
         <div class="row">
             <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12">
-                <h1>Anders Hermansen</h1>
+                <h1>About US</h1>
             </div>
         </div>
 		<div class="row">
@@ -31,9 +31,13 @@ get_header(); ?>
 			<div class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
 				
                 <div class="abount-content">
-                    <p>Industrial Designer Anders Hermansen graduated as a young student from the Royal Danish Academy of Fine Arts, Copenhagen in 1982. Mastering industrial design and furniture architecture.</p>
-                    <p>As a recent graduate, he began working with leadign Danish design companies such Paustian and Louis Poulsen.</p>
-                    <p>Since the early 1980’s Anders Hermansen has also worked closely with the audio-visual company Bang & Olofsen (B&O) for decades.Other close design partnerships includes amonngst others LG Electronics and Engelbrechts Furniture.</p>
+                    <p>Hi! I’m Lars Andersen, founder of Movea.  It’s my mission to inspire people to incorporate cycling into their everyday lives by creating a more exciting and efficient way to explore your town.  It’s not about how far and fast you ride or how may calories you burn, it’s about the fun you have along the way.  My passion and love for ergonomics, style and, ultimately, your smile is what keeps me going.   </p>
+                    <p>Born and raised in Copenhagen, Denmark, been in the bicycle business for more than 25 years, first owning shops and now designing and manufacturing bicycles.  Although I’ve spent much of my life around bikes. </p>
+                    <p>With the new Movea line, we started with a clean slate and distilled all of our design ideas, experience and knowledge from the last two decades into a completely new line of bikes.  Danish designer Anders Hermansen, formerly a designer for Bang and Olufsen, worked with us to create a bicycle with the perfect balance of comfort, style, performance, quality and affordability.  </p>
+                    <p>Our aim is to keep Movea bikes clean and simple but highly functional.  To do this, we’ve incorporated practical rack and cargo solutions and more gears, for a sporty yet smooth and comfortable ride.  Some of our bikes also have electric systems to give you an easy (sweat free!) ride when traveling to work or school.  </p>
+                    <p>Movea is designed to go anywhere with you.  Compact and extremely light, it’s an extraordinarily slim and elegant bicycle that will stand out in a crowd.  But we kept much more than easy storage and looking good in mind.  </p>
+                    <p>Movea’s small wheels are specially designed for riding in traffic.  Because smaller wheels weigh less, they allow for faster acceleration and climb better than larger wheels – a major plus when making frequent stops.  Small wheels are also more responsive to steering, creating a more maneuverable bike. </p>
+                    <p>I’m excited to share my new collection with you.  I hope the new Movea bikes inspire you to get out there and ride.  </p>
                 </div>
 			</div>
 		</div>
